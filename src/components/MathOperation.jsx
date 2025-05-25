08473351587a4c942f9c7a48d23f98bfe730a955
AhmedY29 <ahmedys2001@gmail.com>
2025-05-25 10:45:36 +0300

    complete lab1

---
+++ src/components/MathOperation.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+function MathOperation(props) {
+  const [num1, setNum1] = useState(props.num1);
+  const [num2, setNum2] = useState(props.num2);
+  const [result, setResult] = useState(0);
+  const handleMath = () => {
+    if(props.operation.toLowerCase() == 'sum'){
+      
+      setResult(num1 + num2);
+    }else if(props.operation.toLowerCase() == 'sub'){
+      setResult(num1 - num2);
+
+    }else if(props.operation.toLowerCase() == 'divid'){
+      setResult(num1 / num2);
+
+    }
+    else if(props.operation.toLowerCase() == 'mod'){
+      setResult(num1 % num2);
+
+    }else if(props.operation.toLowerCase() == 'mult'){
+      setResult(num1 * num2);
+
+    }
+  };
+  return (
+    <div>
+      {/* <input type="text" placeholder="Enter Num 1" />
+      <input type="text" placeholder="Enter Num 2" /> */}
+      {num1}, {num2}
+      <hr />
+      <button
+        className="text-white rounded-2xl cursor-pointer my-3 bg-black p-3"
+        onClick={handleMath}
+      >
+        <h1 className="uppercase">{props.operation}</h1>
+      </button>
+        <hr />
+      {result ? result : ""}
+    </div>
+  );
+}
+
+export default MathOperation;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import MathOperation from "../components/MathOperation";
+
+function Home() {
+  return (
+    <div className="flex justify-center w-full">
+      <div className="flex justify-between w-[80%]">
+      <MathOperation operation={"Sum"} num1={10} num2={20} />
+      <MathOperation operation={"sub"} num1={10} num2={20} />
+      <MathOperation operation={"mult"} num1={10} num2={20} />
+      <MathOperation operation={"divid"} num1={10} num2={20} />
+      <MathOperation operation={"mod"} num1={10} num2={20} />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
